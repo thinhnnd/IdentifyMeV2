57570b6c51a6ec6f1d664467ee96267f76db92ef
thupx <xuanthu404@gmail.com>
2020-04-11 15:24:14 +0700

    feat(codebase): init codebase for screens

--- IdentifyMe/IdentifyMe/ViewModels/AboutViewModel.cs
+++ IdentifyMe/IdentifyMe/ViewModels/AboutViewModel.cs
@@ -4,5 +4,12 @@ namespace IdentifyMe.ViewModels
 {
     public class AboutViewModel : BaseNavigationViewModel
     {
+        private string _name = "Test about page";
+
+        public string Name
+        {
+            get => _name;
+            set => RaiseAndUpdate(ref _name, value);
+        }
     }
 }
--- IdentifyMe/IdentifyMe/ViewModels/MainPageViewModel.cs
+++ IdentifyMe/IdentifyMe/ViewModels/MainPageViewModel.cs
@@ -5,10 +5,14 @@ namespace IdentifyMe.ViewModels
     public class MainPageViewModel : BaseNavigationViewModel
     {
         public HomePageViewModel Home { get; set; }
+        public AboutViewModel About { get; set; }
 
-        public MainPageViewModel(HomePageViewModel homePageViewModel)
+        public MainPageViewModel(
+            HomePageViewModel homePageViewModel,
+            AboutViewModel aboutViewModel)
         {
             Home = homePageViewModel;
+            About = aboutViewModel;
         }
     }
 }
