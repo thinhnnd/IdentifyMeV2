11ead3794076d4ccc2c5a86781f2af92bdced62c
thinhnnd <16521179@gm.uit.edu.vn>
2020-05-02 20:36:14 +0700

    change resources

---
+++ IdentifyMe/IdentifyMe/Resources/Themes.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IdentifyMe.Resources
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DefaultThemes : ResourceDictionary
+    {
+        public DefaultThemes()
+        {
+            InitializeComponent();
+        }
+    }
+}
